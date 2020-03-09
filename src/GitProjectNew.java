ec37675e676b6b4215b55761b75130899ba1ac89
okHill <56491314+okHill@users.noreply.github.com>
2020-03-09 18:39:59 -0500

    Update GitProjectNew.java

--- src/GitProjectNew.java
+++ src/GitProjectNew.java
@@ -3,7 +3,8 @@ public class GitProjectNew {
     public static void main(String[] args) {
 
         System.out.println("First");
-
-
+        System.out.println("Second");
+        System.out.println("Third");
+       
     }
 }
