3d471378023b5db7d05e4e5b91dd6cd2ae3af5dd
hilmi <okurhilmi@gmail.com>
2020-03-09 18:43:47 -0500

    First completed

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("First test class");
+        System.out.println("Second test class");
+        System.out.println("Third test class");
     }
 }
