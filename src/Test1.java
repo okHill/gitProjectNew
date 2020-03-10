c5298c5f3103dd2bcfafa9cc6dc9a402ab47ee29
hilmi <okurhilmi@gmail.com>
2020-03-09 20:36:05 -0500

    First completed
    Local added four, five, six

--- src/Test1.java
+++ src/Test1.java
@@ -4,5 +4,9 @@ public class Test1 {
         System.out.println("First test class");
         System.out.println("Second test class");
         System.out.println("Third test class");
+        System.out.println("Local added Fourth test class");
+        System.out.println("Local edded Fifth test class");
+        System.out.println("Local added Sixth class");
+
     }
 }
